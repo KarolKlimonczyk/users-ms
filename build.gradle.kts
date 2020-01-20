b40d44243211aeb4c5cda2aa1a7de21fb3ddc145
Karol Klimonczyk <k.klimonczyk@gmail.com>
2020-01-20 19:48:37 +0100

    enabled actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation(group = "io.jsonwebtoken", name = "jjwt", version = "0.9.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
